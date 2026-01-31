368c2fac551141885b282fac10cbcc02b8ce322d
Avia Naftali <avianaftali@Avias-MacBook-Air.local>
2026-01-31 22:41:40 +0200

    test user

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Express } from "express";
+import mongoose from "mongoose";
+
+import postRouter from "./routes/posts";
+import commentRouter from "./routes/comment";
+import userRouter from "./routes/user";
+import authRouter from "./routes/auth";
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+
+const initApp = () => {
+  const promise = new Promise<Express>((resolve, reject) => {
+    const dbUrl = process.env.DATABASE_URL;
+    if (!dbUrl) {
+      reject("DATABASE_URL is not defined");
+      return;
+    }
+    mongoose.connect(dbUrl, {})
+    .then(() => {
+      resolve(app)}
+    );
+  const db = mongoose.connection;
+  db.on("error", (error) => console.error(error));
+  db.once("open", () => console.log("Connected to Database"));
+  
+});
+  return promise;
+}
+
+export default initApp;
